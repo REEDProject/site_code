c87fd429ae3fea4c98538e8b1352b41bba841929
Jamie Norrish <jamie@artefact.org.nz>
2016-08-29 15:24:09 +1200

    Use app namespaces in URLs rather than manual prefixes.

--- django/text_prepare/urls.py
+++ django/text_prepare/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
-
+app_name = 'text_prepare'
 urlpatterns = [
-    url(r'^convert/$', views.convert, name='text_prepare-convert'),
-    url(r'^update/$', views.update, name='text_prepare-update'),
-    url(r'^validate/$', views.validate, name='text_prepare-validate'),
+    url(r'^convert/$', views.convert, name='convert'),
+    url(r'^update/$', views.update, name='update'),
+    url(r'^validate/$', views.validate, name='validate'),
 ]
