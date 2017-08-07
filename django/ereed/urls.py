eff41dd7de531daa15b8fea75fed685358c3cde7
Jamie Norrish <jamie@artefact.org.nz>
2017-08-08 11:18:02 +1200

    Linked geomap app's urls into Django site.

--- django/ereed/urls.py
+++ django/ereed/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from eats import urls as eats_urls
-from gis import urls as gis_urls
+from geomap import urls as geomap_urls
 from git_update import urls as git_urls
 from records import urls as records_urls
 from text_prepare import urls as text_prepare_urls
@@ -26,7 +26,7 @@ from text_prepare import urls as text_prepare_urls
 urlpatterns = [
     url(r'^accounts/', include('account.urls')),
     url(r'^prepare/', include(text_prepare_urls)),
-    url(r'^gis/', include(gis_urls)),
+    url(r'^geomap/', include(geomap_urls)),
     url(r'^git/', include(git_urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^selectable/', include('selectable.urls')),
