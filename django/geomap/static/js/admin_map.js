13b23b0fc34d44888599199d994993ea5e27853d
Jamie Norrish <jamie@artefact.org.nz>
2018-04-13 12:00:13 +1200

    Changed map code to evaluate property once rather than several times.

--- django/geomap/static/js/admin_map.js
+++ django/geomap/static/js/admin_map.js
@@ -15,18 +15,19 @@ var initialisedMaps = [];
 
 django.jQuery(window).on('map:init', function (e) {
     var detail = e.originalEvent ? e.originalEvent.detail : e.detail;
+    var map = detail.map;
     // map:init is sent multiple times for each map (perhaps as many
     // times for each as there are maps?), so to avoid both having
     // multiple co-ordinate display boxes and also doing extra work,
     // keep track of which maps have already been initialised.
-    if (initialisedMaps.indexOf(detail.map._containerId) < 0) {
+    if (initialisedMaps.indexOf(map._containerId) < 0) {
         L.geoJson(geoData, {
             pointToLayer: function (feature, latlng) {
                 return L.circleMarker(latlng, geojsonMarkerOptions);
             },
             onEachFeature: onEachFeature
-        }).addTo(detail.map);
-        L.control.mouseCoordinate({gpsLong:false}).addTo(detail.map);
-        initialisedMaps.push(detail.map._containerId);
+        }).addTo(map);
+        L.control.mouseCoordinate({gpsLong:false}).addTo(map);
+        initialisedMaps.push(map._containerId);
     }
 });
