9388fcfbe2cc8c8e7ad8eb1858f7e3f9cd53848f
inokhrin <illya.nokhrin@mail.utoronto.ca>
2020-07-20 17:12:01 -0400

    preparing dev site update

--- kiln/webapps/ROOT/assets/scripts/leaflet/regions/1787.js
+++ kiln/webapps/ROOT/assets/scripts/leaflet/regions/1787.js
@@ -2,5 +2,5 @@ var source_region_geojson = [{
 "type":"FeatureCollection"
 ,"crs":{"type":"name","properties":{"name":"EPSG:4326"}}
 ,"features":[
-{"geometry": {"coordinates": [[[[-0.08944732525315126, 51.5059372868655], [-0.08955720358542321, 51.505760824802906], [-0.08986608480429692, 51.505835992109105], [-0.08988343217074582, 51.50587543178865], [-0.09029432786990041, 51.50597830491262], [-0.09023356223084038, 51.50611576673789], [-0.08944732525315126, 51.5059372868655]]]], "type": "MultiPolygon"}, "type": "Feature", "properties": {"container": 643, "place_type": "property", "symbol_flag": 0, "placeholder_coordinates": null, "patrons_label_flag": 0, "is_approximate": false, "pk": "1787", "notes": "", "name": "St Saviour's Churchyard"}}
+{"geometry": {"coordinates": [[[[-0.08944732525315126, 51.5059372868655], [-0.08955720358542321, 51.505760824802906], [-0.08986608480429692, 51.505835992109105], [-0.08988343217074582, 51.50587543178865], [-0.09029432786990041, 51.50597830491262], [-0.09023356223084038, 51.50611576673789], [-0.08944732525315126, 51.5059372868655]]]], "type": "MultiPolygon"}, "properties": {"pk": "1787", "notes": "", "name": "St Saviour's Churchyard", "symbol_flag": 0, "place_type": "property", "container": 643, "is_approximate": false, "patrons_label_flag": 0, "placeholder_coordinates": null}, "type": "Feature"}
 ]}];
--- kiln/webapps/ROOT/assets/scripts/leaflet/regions/1797.js
+++ kiln/webapps/ROOT/assets/scripts/leaflet/regions/1797.js
@@ -2,5 +2,5 @@ var source_region_geojson = [{
 "type":"FeatureCollection"
 ,"crs":{"type":"name","properties":{"name":"EPSG:4326"}}
 ,"features":[
-{"geometry": {"coordinates": [[[[-0.09965804748964606, 51.50833190422245], [-0.09945287677034002, 51.50831147258176], [-0.0994364627534754, 51.508193989491375], [-0.09964163347278143, 51.5081837739208], [-0.09965804748964606, 51.50833190422245]]]], "type": "MultiPolygon"}, "type": "Feature", "properties": {"container": 643, "place_type": "property", "symbol_flag": 0, "placeholder_coordinates": null, "patrons_label_flag": 0, "is_approximate": false, "pk": "1797", "notes": "", "name": "Henslowe property"}}
+{"geometry": {"coordinates": [[[[-0.09965804748964606, 51.50833190422245], [-0.09945287677034002, 51.50831147258176], [-0.0994364627534754, 51.508193989491375], [-0.09964163347278143, 51.5081837739208], [-0.09965804748964606, 51.50833190422245]]]], "type": "MultiPolygon"}, "properties": {"pk": "1797", "notes": "", "name": "Henslowe property", "symbol_flag": 0, "place_type": "property", "container": 643, "is_approximate": false, "patrons_label_flag": 0, "placeholder_coordinates": null}, "type": "Feature"}
 ]}];
