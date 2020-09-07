410df5efe0d4413ff81d03a8e94fc08c021e0d96
inokhrin <illya.nokhrin@mail.utoronto.ca>
2020-09-07 09:52:07 -0400

    Mapping update in response to Byron's request

--- kiln/webapps/ROOT/assets/scripts/leaflet/regions/1787.js
+++ kiln/webapps/ROOT/assets/scripts/leaflet/regions/1787.js
@@ -2,5 +2,5 @@ var source_region_geojson = [{
 "type":"FeatureCollection"
 ,"crs":{"type":"name","properties":{"name":"EPSG:4326"}}
 ,"features":[
-{"geometry": {"coordinates": [[[[-0.08944732525315126, 51.5059372868655], [-0.08955720358542321, 51.505760824802906], [-0.08986608480429692, 51.505835992109105], [-0.08988343217074582, 51.50587543178865], [-0.09029432786990041, 51.50597830491262], [-0.09023356223084038, 51.50611576673789], [-0.08944732525315126, 51.5059372868655]]]], "type": "MultiPolygon"}, "properties": {"pk": "1787", "notes": "", "name": "St Saviour's Churchyard", "symbol_flag": 0, "place_type": "property", "container": 643, "is_approximate": false, "patrons_label_flag": 0, "placeholder_coordinates": null}, "type": "Feature"}
+{"type": "Feature", "properties": {"name": "St Saviour's Churchyard", "place_type": "property", "is_approximate": false, "placeholder_coordinates": null, "container": 643, "patrons_label_flag": 0, "symbol_flag": 0, "notes": "", "pk": "1787"}, "geometry": {"type": "MultiPolygon", "coordinates": [[[[-0.08944732525315126, 51.5059372868655], [-0.08955720358542321, 51.505760824802906], [-0.08986608480429692, 51.505835992109105], [-0.08988343217074582, 51.50587543178865], [-0.09029432786990041, 51.50597830491262], [-0.09023356223084038, 51.50611576673789], [-0.08944732525315126, 51.5059372868655]]]]}}
 ]}];
