df71934c7f24df1983fa519a4662d7e78858d32b
Illya Nokhrin <illya.nokhrin@gmail.com>
2023-07-19 12:49:55 -0400

    updates to region files

---
+++ kiln/webapps/ROOT/assets/scripts/leaflet/regions/2149.js
@@ -0,0 +1,6 @@
+var source_region_geojson = [{
+"type":"FeatureCollection"
+,"crs":{"type":"name","properties":{"name":"EPSG:4326"}}
+,"features":[
+{"type": "Feature", "properties": {"name": "New Rents", "place_type": "other site", "is_approximate": false, "placeholder_coordinates": null, "container": 643, "patrons_label_flag": 0, "symbol_flag": 0, "notes": "Polygon used to highlight street in Timeline view", "pk": "2149"}, "geometry": {"type": "Polygon", "coordinates": [[[-0.09027779102325441, 51.50622494301392], [-0.09032070636749268, 51.505954485430635], [-0.09036362171173096, 51.50574746743038], [-0.09035289287567139, 51.50568068722968], [-0.09035289287567139, 51.505674009204185], [-0.09046018123626709, 51.505660653150386], [-0.09047627449035645, 51.505744128422656], [-0.09043335914611818, 51.50596116341497], [-0.09039312601089498, 51.50622893412941], [-0.09027779102325441, 51.50622494301392]]]}}
+]}];
---
+++ kiln/webapps/ROOT/assets/scripts/leaflet/regions/2153.js
@@ -0,0 +1,6 @@
+var source_region_geojson = [{
+"type":"FeatureCollection"
+,"crs":{"type":"name","properties":{"name":"EPSG:4326"}}
+,"features":[
+{"type": "Feature", "properties": {"name": "Sewer south of Lurklane property", "place_type": "other site", "is_approximate": false, "placeholder_coordinates": null, "container": 13, "patrons_label_flag": 0, "symbol_flag": 0, "notes": "", "pk": "2153"}, "geometry": {"type": "MultiPolygon", "coordinates": [[[[-0.09913641005432583, 51.494369004838816], [-0.09913008794507248, 51.494346631954116], [-0.0991235093657771, 51.4943233514531], [-0.09982654887067481, 51.49428447184786], [-0.09983277857159141, 51.49430651774525], [-0.09983944955922355, 51.494330125272526], [-0.09913641005432583, 51.494369004838816]]]]}}
+]}];
