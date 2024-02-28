d62ae77b8607c636fc5662053e9d92f0f73afd86
Illya Nokhrin <illya.nokhrin@gmail.com>
2024-02-28 17:53:38 -0500

    Revert "geo regions update in anticipation of BGHope launch"
    This reverts commit 8829fa737c0648544370010cbfdc4b8c6ee4359a.

--- kiln/webapps/ROOT/assets/scripts/leaflet/regions/1787.js
+++ kiln/webapps/ROOT/assets/scripts/leaflet/regions/1787.js
@@ -2,5 +2,5 @@ var source_region_geojson = [{
 "type":"FeatureCollection"
 ,"crs":{"type":"name","properties":{"name":"EPSG:4326"}}
 ,"features":[
-{"type": "Feature", "properties": {"name": "St Saviour's Churchyard", "place_type": "property", "is_approximate": false, "placeholder_coordinates": null, "container": 643, "patrons_label_flag": 0, "symbol_flag": 0, "notes": "", "pk": "1787"}, "geometry": {"type": "MultiPolygon", "coordinates": [[[[51.50593728686552, -0.08944732525315127], [51.50576082480292, -0.08955720358542324], [51.50583599210912, -0.08986608480429693], [51.50587543178867, -0.08988343217074585], [51.50597830491264, -0.09029432786990042], [51.50611576673791, -0.0902335622308404], [51.50593728686552, -0.08944732525315127]]]]}}
+{"type": "Feature", "properties": {"name": "St Saviour's Churchyard", "place_type": "property", "is_approximate": false, "placeholder_coordinates": null, "container": 643, "patrons_label_flag": 0, "symbol_flag": 0, "notes": "", "pk": "1787"}, "geometry": {"type": "MultiPolygon", "coordinates": [[[[-0.08944732525315126, 51.5059372868655], [-0.08955720358542321, 51.505760824802906], [-0.08986608480429692, 51.505835992109105], [-0.08988343217074582, 51.50587543178865], [-0.09029432786990041, 51.50597830491262], [-0.09023356223084038, 51.50611576673789], [-0.08944732525315126, 51.5059372868655]]]]}}
 ]}];
--- kiln/webapps/ROOT/assets/scripts/leaflet/regions/2152.js
+++ kiln/webapps/ROOT/assets/scripts/leaflet/regions/2152.js
@@ -2,5 +2,5 @@ var source_region_geojson = [{
 "type":"FeatureCollection"
 ,"crs":{"type":"name","properties":{"name":"EPSG:4326"}}
 ,"features":[
-{"type": "Feature", "properties": {"name": "Lurklane", "place_type": "property", "is_approximate": false, "placeholder_coordinates": null, "container": 13, "patrons_label_flag": 2, "symbol_flag": 2, "notes": "", "pk": "2152"}, "geometry": {"type": "MultiPolygon", "coordinates": [[[[51.49462122276463, -0.09992929992008145], [51.4947136204279, -0.09923878456775777], [51.49436900483883, -0.09913641005432586], [51.49433012527254, -0.09983944955922358], [51.49462122276463, -0.09992929992008145]]]]}}
+{"type": "Feature", "properties": {"name": "Lurklane", "place_type": "property", "is_approximate": false, "placeholder_coordinates": null, "container": 13, "patrons_label_flag": 2, "symbol_flag": 2, "notes": "", "pk": "2152"}, "geometry": {"type": "MultiPolygon", "coordinates": [[[[-0.09992929992008143, 51.494621222764614], [-0.09923878456775774, 51.49471362042787], [-0.09913641005432583, 51.494369004838816], [-0.09983944955922355, 51.494330125272526], [-0.09992929992008143, 51.494621222764614]]]]}}
 ]}];
