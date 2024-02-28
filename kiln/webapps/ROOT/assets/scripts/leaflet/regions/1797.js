8829fa737c0648544370010cbfdc4b8c6ee4359a
Illya Nokhrin <illya.nokhrin@gmail.com>
2024-02-28 17:14:28 -0500

    geo regions update in anticipation of BGHope launch

--- kiln/webapps/ROOT/assets/scripts/leaflet/regions/1797.js
+++ kiln/webapps/ROOT/assets/scripts/leaflet/regions/1797.js
@@ -2,5 +2,5 @@ var source_region_geojson = [{
 "type":"FeatureCollection"
 ,"crs":{"type":"name","properties":{"name":"EPSG:4326"}}
 ,"features":[
-{"type": "Feature", "properties": {"name": "Henslowe property", "place_type": "property", "is_approximate": false, "placeholder_coordinates": null, "container": 643, "patrons_label_flag": 0, "symbol_flag": 0, "notes": "", "pk": "1797"}, "geometry": {"type": "MultiPolygon", "coordinates": [[[[-0.09965804748964606, 51.50833190422245], [-0.09945287677034002, 51.50831147258176], [-0.0994364627534754, 51.508193989491375], [-0.09964163347278143, 51.5081837739208], [-0.09965804748964606, 51.50833190422245]]]]}}
+{"type": "Feature", "properties": {"name": "Henslowe property", "place_type": "property", "is_approximate": false, "placeholder_coordinates": null, "container": 643, "patrons_label_flag": 0, "symbol_flag": 0, "notes": "", "pk": "1797"}, "geometry": {"type": "MultiPolygon", "coordinates": [[[[51.508331904222466, -0.09965804748964609], [51.508311472581774, -0.09945287677034005], [51.508193989491396, -0.09943646275347542], [51.50818377392081, -0.09964163347278146], [51.508331904222466, -0.09965804748964609]]]]}}
 ]}];
--- kiln/webapps/ROOT/assets/scripts/leaflet/regions/2153.js
+++ kiln/webapps/ROOT/assets/scripts/leaflet/regions/2153.js
@@ -2,5 +2,5 @@ var source_region_geojson = [{
 "type":"FeatureCollection"
 ,"crs":{"type":"name","properties":{"name":"EPSG:4326"}}
 ,"features":[
-{"type": "Feature", "properties": {"name": "Sewer south of Lurklane property", "place_type": "other site", "is_approximate": false, "placeholder_coordinates": null, "container": 13, "patrons_label_flag": 0, "symbol_flag": 0, "notes": "", "pk": "2153"}, "geometry": {"type": "MultiPolygon", "coordinates": [[[[-0.09913641005432583, 51.494369004838816], [-0.09913008794507248, 51.494346631954116], [-0.0991235093657771, 51.4943233514531], [-0.09982654887067481, 51.49428447184786], [-0.09983277857159141, 51.49430651774525], [-0.09983944955922355, 51.494330125272526], [-0.09913641005432583, 51.494369004838816]]]]}}
+{"type": "Feature", "properties": {"name": "Sewer south of Lurklane property", "place_type": "other site", "is_approximate": false, "placeholder_coordinates": null, "container": 13, "patrons_label_flag": 0, "symbol_flag": 0, "notes": "", "pk": "2153"}, "geometry": {"type": "MultiPolygon", "coordinates": [[[[51.49436900483883, -0.09913641005432586], [51.49434663195413, -0.09913008794507251], [51.49432335145311, -0.09912350936577713], [51.49428447184788, -0.09982654887067482], [51.494306517745265, -0.09983277857159144], [51.49433012527254, -0.09983944955922358], [51.49436900483883, -0.09913641005432586]]]]}}
 ]}];
