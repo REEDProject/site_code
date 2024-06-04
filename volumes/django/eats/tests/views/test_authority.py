b9d136677e26c0c7bc50e1b898ac481e86d03f0f
Miguel Vieira <jose.m.vieira@kcl.ac.uk>
2024-06-04 21:47:03 +0100

    test(eats): fix tests

--- volumes/django/eats/tests/views/test_authority.py
+++ volumes/django/eats/tests/views/test_authority.py
@@ -5,96 +5,107 @@ from eats.models import Authority
 from .admin_view_test_case import AdminViewTestCase
 
 
-class AuthorityViewsTestCase (AdminViewTestCase):
+class AuthorityViewsTestCase(AdminViewTestCase):
 
-    def test_authentication (self):
+    def test_authentication(self):
         url_data = [
-            ('eats-authority-list', {}),
-            ('eats-authority-add', {}),
-            ('eats-authority-change', {'topic_id': self.authority.get_id()})]
+            ("eats-authority-list", {}),
+            ("eats-authority-add", {}),
+            ("eats-authority-change", {"topic_id": self.authority.get_id()}),
+        ]
         for url_name, kwargs in url_data:
             url = reverse(url_name, kwargs=kwargs)
-            login_url = settings.LOGIN_URL + '?next=' + url
+            login_url = settings.LOGIN_URL + "?next=" + url
             response = self.app.get(url)
-            self.assertRedirects(response, login_url,
-                                 msg_prefix='Anonymous user to {}'.format(
-                                     url_name))
+            self.assertRedirects(
+                response, login_url, msg_prefix="Anonymous user to {}".format(url_name)
+            )
         for url_name, kwargs in url_data:
-            response = self.app.get(url, user='user')
-            self.assertRedirects(response, login_url,
-                                 msg_prefix='User to {}'.format(url_name))
+            response = self.app.get(url, user="user")
+            self.assertRedirects(
+                response,
+                login_url,
+                msg_prefix="User to {}".format(url_name),
+                target_status_code=302,
+            )
         for url_name, kwargs in url_data:
-            response = self.app.get(url, user='eats_user')
-            self.assertRedirects(response, login_url,
-                                 msg_prefix='EATS user to {}'.format(url_name))
-            response = self.app.get(url, user='staff')
+            response = self.app.get(url, user="eats_user")
+            self.assertRedirects(
+                response,
+                login_url,
+                msg_prefix="EATS user to {}".format(url_name),
+                target_status_code=302,
+            )
+            response = self.app.get(url, user="staff")
             self.assertEqual(response.status_code, 200)
 
-    def test_authority_list (self):
-        url = reverse('eats-authority-list')
-        response = self.app.get(url, user='staff')
-        self.assertEqual(response.context['opts'], Authority._meta)
-        self.assertEqual(len(response.context['topics']), 1)
-        self.assertTrue(self.authority in response.context['topics'])
-        authority2 = self.create_authority('Test2')
-        response = self.app.get(url, user='staff')
-        self.assertEqual(len(response.context['topics']), 2)
-        self.assertTrue(self.authority in response.context['topics'])
-        self.assertTrue(authority2 in response.context['topics'])
+    def test_authority_list(self):
+        url = reverse("eats-authority-list")
+        response = self.app.get(url, user="staff")
+        self.assertEqual(response.context["opts"], Authority._meta)
+        self.assertEqual(len(response.context["topics"]), 1)
+        self.assertTrue(self.authority in response.context["topics"])
+        authority2 = self.create_authority("Test2")
+        response = self.app.get(url, user="staff")
+        self.assertEqual(len(response.context["topics"]), 2)
+        self.assertTrue(self.authority in response.context["topics"])
+        self.assertTrue(authority2 in response.context["topics"])
 
-    def test_authority_add_get (self):
-        url = reverse('eats-authority-add')
-        response = self.app.get(url, user='staff')
-        self.assertEqual(response.context['opts'], Authority._meta)
+    def test_authority_add_get(self):
+        url = reverse("eats-authority-add")
+        response = self.app.get(url, user="staff")
+        self.assertEqual(response.context["opts"], Authority._meta)
 
-    def test_authority_add_post_redirects (self):
-        url = reverse('eats-authority-add')
-        form = self.app.get(url, user='staff').forms['infrastructure-add-form']
-        form['name'] = 'Test2'
-        response = form.submit('_save')
-        self.assertRedirects(response, reverse('eats-authority-list'))
+    def test_authority_add_post_redirects(self):
+        url = reverse("eats-authority-add")
+        form = self.app.get(url, user="staff").forms["infrastructure-add-form"]
+        form["name"] = "Test2"
+        response = form.submit("_save")
+        self.assertRedirects(response, reverse("eats-authority-list"))
         self.assertEqual(Authority.objects.count(), 2)
-        form = self.app.get(url, user='staff').forms['infrastructure-add-form']
-        form['name'] = 'Test3'
-        response = form.submit('_addanother')
+        form = self.app.get(url, user="staff").forms["infrastructure-add-form"]
+        form["name"] = "Test3"
+        response = form.submit("_addanother")
         self.assertRedirects(response, url)
         self.assertEqual(Authority.objects.count(), 3)
-        form = self.app.get(url, user='staff').forms['infrastructure-add-form']
-        form['name'] = 'Test4'
-        response = form.submit('_continue')
-        authority = Authority.objects.get_by_admin_name('Test4')
-        redirect_url = reverse('eats-authority-change',
-                               kwargs={'topic_id': authority.get_id()})
+        form = self.app.get(url, user="staff").forms["infrastructure-add-form"]
+        form["name"] = "Test4"
+        response = form.submit("_continue")
+        authority = Authority.objects.get_by_admin_name("Test4")
+        redirect_url = reverse(
+            "eats-authority-change", kwargs={"topic_id": authority.get_id()}
+        )
         self.assertRedirects(response, redirect_url)
         self.assertEqual(Authority.objects.count(), 4)
 
-    def test_authority_add_post_content (self):
-        url = reverse('eats-authority-add')
-        calendar1 = self.create_calendar('Test1')
-        calendar2 = self.create_calendar('Test2')
-        date_period = self.create_date_period('Test')
-        date_type = self.create_date_type('Test')
+    def test_authority_add_post_content(self):
+        url = reverse("eats-authority-add")
+        calendar1 = self.create_calendar("Test1")
+        calendar2 = self.create_calendar("Test2")
+        date_period = self.create_date_period("Test")
+        date_type = self.create_date_type("Test")
         entity_relationship_type = self.create_entity_relationship_type(
-            'Test', 'Reverse')
-        entity_type = self.create_entity_type('Test')
-        language = self.create_language('English', 'en')
-        name_type = self.create_name_type('Test')
-        script = self.create_script('Latin', 'Latn', ' ')
-        user = self.create_django_user('user1', 'user1@example.org', 'password')
+            "Test", "Reverse"
+        )
+        entity_type = self.create_entity_type("Test")
+        language = self.create_language("English", "en")
+        name_type = self.create_name_type("Test")
+        script = self.create_script("Latin", "Latn", " ")
+        user = self.create_django_user("user1", "user1@example.org", "password")
         editor = self.create_user(user)
-        form = self.app.get(url, user='staff').forms['infrastructure-add-form']
-        form['name'] = 'Test1'
-        form['calendars'] = [calendar1.get_id(), calendar2.get_id()]
-        form['date_periods'] = [date_period.get_id()]
-        form['date_types'] = [date_type.get_id()]
-        form['entity_relationship_types'] = [entity_relationship_type.get_id()]
-        form['entity_types'] = [entity_type.get_id()]
-        form['languages'] = [language.get_id()]
-        form['name_types'] = [name_type.get_id()]
-        form['scripts'] = [script.get_id()]
-        form['editors'] = [editor.pk]
-        form.submit('_save')
-        authority = Authority.objects.get_by_admin_name('Test1')
+        form = self.app.get(url, user="staff").forms["infrastructure-add-form"]
+        form["name"] = "Test1"
+        form["calendars"] = [calendar1.get_id(), calendar2.get_id()]
+        form["date_periods"] = [date_period.get_id()]
+        form["date_types"] = [date_type.get_id()]
+        form["entity_relationship_types"] = [entity_relationship_type.get_id()]
+        form["entity_types"] = [entity_type.get_id()]
+        form["languages"] = [language.get_id()]
+        form["name_types"] = [name_type.get_id()]
+        form["scripts"] = [script.get_id()]
+        form["editors"] = [editor.pk]
+        form.submit("_save")
+        authority = Authority.objects.get_by_admin_name("Test1")
         self.assertEqual(len(authority.get_calendars()), 2)
         self.assertTrue(calendar1 in authority.get_calendars())
         self.assertTrue(calendar2 in authority.get_calendars())
@@ -103,8 +114,9 @@ class AuthorityViewsTestCase (AdminViewTestCase):
         self.assertEqual(len(authority.get_date_types()), 1)
         self.assertTrue(date_type in authority.get_date_types())
         self.assertEqual(len(authority.get_entity_relationship_types()), 1)
-        self.assertTrue(entity_relationship_type in
-                        authority.get_entity_relationship_types())
+        self.assertTrue(
+            entity_relationship_type in authority.get_entity_relationship_types()
+        )
         self.assertEqual(len(authority.get_entity_types()), 1)
         self.assertTrue(entity_type in authority.get_entity_types())
         self.assertEqual(len(authority.get_languages()), 1)
@@ -116,31 +128,33 @@ class AuthorityViewsTestCase (AdminViewTestCase):
         self.assertEqual(len(authority.get_editors()), 1)
         self.assertTrue(editor in authority.get_editors())
 
-    def test_authority_add_illegal_post (self):
+    def test_authority_add_illegal_post(self):
         self.assertEqual(Authority.objects.count(), 1)
-        url = reverse('eats-authority-add')
-        form = self.app.get(url, user='staff').forms['infrastructure-add-form']
-        form['name'] = 'Test'
-        response = form.submit('_save')
+        url = reverse("eats-authority-add")
+        form = self.app.get(url, user="staff").forms["infrastructure-add-form"]
+        form["name"] = "Test"
+        response = form.submit("_save")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(Authority.objects.count(), 1)
 
-    def test_authority_change_illegal_get (self):
-        url = reverse('eats-authority-change', kwargs={'topic_id': 0})
-        self.app.get(url, status=404, user='staff')
+    def test_authority_change_illegal_get(self):
+        url = reverse("eats-authority-change", kwargs={"topic_id": 0})
+        self.app.get(url, status=404, user="staff")
 
-    def test_authority_change_get (self):
-        url = reverse('eats-authority-change', kwargs={
-                'topic_id': self.authority.get_id()})
-        response = self.app.get(url, user='staff')
+    def test_authority_change_get(self):
+        url = reverse(
+            "eats-authority-change", kwargs={"topic_id": self.authority.get_id()}
+        )
+        response = self.app.get(url, user="staff")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context['form'].instance, self.authority)
+        self.assertEqual(response.context["form"].instance, self.authority)
 
-    def test_authority_change_post (self):
+    def test_authority_change_post(self):
         self.assertEqual(Authority.objects.count(), 1)
-        url = reverse('eats-authority-change', kwargs={
-                'topic_id': self.authority.get_id()})
-        self.assertEqual(self.authority.get_admin_name(), 'Test')
+        url = reverse(
+            "eats-authority-change", kwargs={"topic_id": self.authority.get_id()}
+        )
+        self.assertEqual(self.authority.get_admin_name(), "Test")
         self.assertEqual(len(self.authority.get_calendars()), 0)
         self.assertEqual(len(self.authority.get_date_periods()), 0)
         self.assertEqual(len(self.authority.get_date_types()), 0)
@@ -150,38 +164,36 @@ class AuthorityViewsTestCase (AdminViewTestCase):
         self.assertEqual(len(self.authority.get_name_types()), 0)
         self.assertEqual(len(self.authority.get_scripts()), 0)
         self.assertEqual(len(self.authority.get_editors()), 0)
-        calendar1 = self.create_calendar('Test1')
-        calendar2 = self.create_calendar('Test2')
-        date_period = self.create_date_period('Test')
-        date_type = self.create_date_type('Test')
+        calendar1 = self.create_calendar("Test1")
+        calendar2 = self.create_calendar("Test2")
+        date_period = self.create_date_period("Test")
+        date_type = self.create_date_type("Test")
         entity_relationship_type = self.create_entity_relationship_type(
-            'Test', 'Reverse')
-        entity_type = self.create_entity_type('Test')
-        language = self.create_language('English', 'en')
-        name_type = self.create_name_type('Test')
-        script = self.create_script('Latin', 'Latn', ' ')
-        user1 = self.create_django_user('user1', 'user1@example.org',
-                                        'password')
+            "Test", "Reverse"
+        )
+        entity_type = self.create_entity_type("Test")
+        language = self.create_language("English", "en")
+        name_type = self.create_name_type("Test")
+        script = self.create_script("Latin", "Latn", " ")
+        user1 = self.create_django_user("user1", "user1@example.org", "password")
         editor1 = self.create_user(user1)
-        user2 = self.create_django_user('user2', 'user2@example.org',
-                                        'password')
+        user2 = self.create_django_user("user2", "user2@example.org", "password")
         editor2 = self.create_user(user2)
-        form = self.app.get(url, user='staff').forms[
-            'infrastructure-change-form']
-        form['name'] = 'Test1'
-        form['calendars'] = [calendar1.get_id(), calendar2.get_id()]
-        form['date_periods'] = [date_period.get_id()]
-        form['date_types'] = [date_type.get_id()]
-        form['entity_relationship_types'] = [entity_relationship_type.get_id()]
-        form['entity_types'] = [entity_type.get_id()]
-        form['languages'] = [language.get_id()]
-        form['name_types'] = [name_type.get_id()]
-        form['scripts'] = [script.get_id()]
-        form['editors'] = [editor1.pk, editor2.pk]
-        response = form.submit('_save')
-        self.assertRedirects(response, reverse('eats-authority-list'))
+        form = self.app.get(url, user="staff").forms["infrastructure-change-form"]
+        form["name"] = "Test1"
+        form["calendars"] = [calendar1.get_id(), calendar2.get_id()]
+        form["date_periods"] = [date_period.get_id()]
+        form["date_types"] = [date_type.get_id()]
+        form["entity_relationship_types"] = [entity_relationship_type.get_id()]
+        form["entity_types"] = [entity_type.get_id()]
+        form["languages"] = [language.get_id()]
+        form["name_types"] = [name_type.get_id()]
+        form["scripts"] = [script.get_id()]
+        form["editors"] = [editor1.pk, editor2.pk]
+        response = form.submit("_save")
+        self.assertRedirects(response, reverse("eats-authority-list"))
         self.assertEqual(Authority.objects.count(), 1)
-        self.assertEqual(self.authority.get_admin_name(), 'Test1')
+        self.assertEqual(self.authority.get_admin_name(), "Test1")
         self.assertEqual(len(self.authority.get_calendars()), 2)
         self.assertTrue(calendar1 in self.authority.get_calendars())
         self.assertTrue(calendar2 in self.authority.get_calendars())
@@ -190,8 +202,9 @@ class AuthorityViewsTestCase (AdminViewTestCase):
         self.assertEqual(len(self.authority.get_date_types()), 1)
         self.assertTrue(date_type in self.authority.get_date_types())
         self.assertEqual(len(self.authority.get_entity_relationship_types()), 1)
-        self.assertTrue(entity_relationship_type in
-                        self.authority.get_entity_relationship_types())
+        self.assertTrue(
+            entity_relationship_type in self.authority.get_entity_relationship_types()
+        )
         self.assertEqual(len(self.authority.get_entity_types()), 1)
         self.assertTrue(entity_type in self.authority.get_entity_types())
         self.assertEqual(len(self.authority.get_languages()), 1)
@@ -206,24 +219,24 @@ class AuthorityViewsTestCase (AdminViewTestCase):
         self.assertTrue(self.authority in editor1.editable_authorities.all())
         self.assertTrue(self.authority in editor2.editable_authorities.all())
 
-    def test_authority_illegal_remove (self):
+    def test_authority_illegal_remove(self):
         """Tests that an infrastructure element in use cannot be removed."""
-        calendar = self.create_calendar('Julian')
-        entity_type1 = self.create_entity_type('Test1')
-        entity_type2 = self.create_entity_type('Test2')
+        calendar = self.create_calendar("Julian")
+        entity_type1 = self.create_entity_type("Test1")
+        entity_type2 = self.create_entity_type("Test2")
         self.authority.set_entity_types([entity_type1, entity_type2])
         self.assertEqual(len(self.authority.get_calendars()), 0)
         entity = self.tm.create_entity(self.authority)
-        entity.create_entity_type_property_assertion(self.authority,
-                                                     entity_type1)
-        url = reverse('eats-authority-change', kwargs={
-                'topic_id': self.authority.get_id()})
-        form = self.app.get(url, user='staff').forms[
-            'infrastructure-change-form']
-        form['calendars'] = [calendar.get_id()]
-        form['entity_types'] = [entity_type2.get_id()]
-        response = form.submit('_save')
+        entity.create_entity_type_property_assertion(self.authority, entity_type1)
+        url = reverse(
+            "eats-authority-change", kwargs={"topic_id": self.authority.get_id()}
+        )
+        form = self.app.get(url, user="staff").forms["infrastructure-change-form"]
+        form["calendars"] = [calendar.get_id()]
+        form["entity_types"] = [entity_type2.get_id()]
+        response = form.submit("_save")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(set(self.authority.get_entity_types()),
-                         set([entity_type1, entity_type2]))
+        self.assertEqual(
+            set(self.authority.get_entity_types()), set([entity_type1, entity_type2])
+        )
         self.assertEqual(len(self.authority.get_calendars()), 0)
--- volumes/django/eats/tests/views/test_entity_add.py
+++ volumes/django/eats/tests/views/test_entity_add.py
@@ -5,52 +5,51 @@ from eats.models import EATSUser, Entity
 from eats.tests.views.view_test_case import ViewTestCase
 
 
-class EntityAddViewTestCase (ViewTestCase):
+class EntityAddViewTestCase(ViewTestCase):
 
-    def setUp (self):
+    def setUp(self):
         super(EntityAddViewTestCase, self).setUp()
-        user = self.create_django_user('user', 'user@example.org', 'password')
+        user = self.create_django_user("user", "user@example.org", "password")
         self.editor = self.create_user(user)
         self.editor.editable_authorities = [self.authority]
         self.editor.set_current_authority(self.authority)
 
-    def test_authentication (self):
-        url = reverse('eats-entity-add')
-        login_url = settings.LOGIN_URL + '?next=' + url
+    def test_authentication(self):
+        url = reverse("eats-entity-add")
+        login_url = settings.LOGIN_URL + "?next=" + url
         response = self.app.get(url)
         self.assertRedirects(response, login_url)
-        user = self.create_django_user('user2', 'user2@example.org', 'password')
-        response = self.app.get(url, user='user2')
-        self.assertRedirects(response, login_url)
+        user = self.create_django_user("user2", "user2@example.org", "password")
+        response = self.app.get(url, user="user2")
+        self.assertRedirects(response, login_url, target_status_code=302)
         self.create_user(user)
-        response = self.app.get(url, user='user2')
-        self.assertRedirects(response, login_url)
-        response = self.app.get(url, user='user')
+        response = self.app.get(url, user="user2")
+        self.assertRedirects(response, login_url, target_status_code=302)
+        response = self.app.get(url, user="user")
         self.assertEqual(response.status_code, 200)
 
-    def test_add_entity (self):
+    def test_add_entity(self):
         self.assertEqual(Entity.objects.count(), 0)
-        url = reverse('eats-entity-add')
-        form = self.app.get(url, user='user').forms['entity-add-form']
-        form['authority'] = self.authority.get_id()
-        response = form.submit('_save')
+        url = reverse("eats-entity-add")
+        form = self.app.get(url, user="user").forms["entity-add-form"]
+        form["authority"] = self.authority.get_id()
+        response = form.submit("_save")
         self.assertEqual(Entity.objects.count(), 1)
         entity = Entity.objects.all()[0]
         entity_id = entity.get_id()
-        redirect_url = reverse('eats-entity-change',
-                               kwargs={'entity_id': entity_id})
+        redirect_url = reverse("eats-entity-change", kwargs={"entity_id": entity_id})
         self.assertRedirects(response, redirect_url)
         self.assertEqual(entity.creator, self.editor.user)
 
-    def test_change_current_authority (self):
-        authority = self.create_authority('Test2')
+    def test_change_current_authority(self):
+        authority = self.create_authority("Test2")
         self.editor.editable_authorities = [self.authority, authority]
         self.assertEqual(self.editor.get_current_authority(), self.authority)
         self.assertEqual(Entity.objects.count(), 0)
-        url = reverse('eats-entity-add')
-        form = self.app.get(url, user='user').forms['entity-add-form']
-        form['authority'] = authority.get_id()
-        response = form.submit('_save')
+        url = reverse("eats-entity-add")
+        form = self.app.get(url, user="user").forms["entity-add-form"]
+        form["authority"] = authority.get_id()
+        response = form.submit("_save")
         self.assertEqual(Entity.objects.count(), 1)
         entity_id = Entity.objects.all()[0].get_id()
         # self.editor is out-of-date, since the view has changed the
@@ -58,6 +57,5 @@ class EntityAddViewTestCase (ViewTestCase):
         # object.
         editor = EATSUser.objects.get(pk=self.editor.pk)
         self.assertEqual(editor.get_current_authority(), authority)
-        redirect_url = reverse('eats-entity-change',
-                               kwargs={'entity_id': entity_id})
+        redirect_url = reverse("eats-entity-change", kwargs={"entity_id": entity_id})
         self.assertRedirects(response, redirect_url)
